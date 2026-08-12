c5a2fa072b321ed2de1b4ea9ba7a51a14d962ae2
Amol Varule <amol.varule@writerrelocations.com>
2026-08-12 12:06:02 +0530

    Add project files.

---
+++ DemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DemoMVC.Models;
+
+namespace DemoMVC.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new Employee());
+    }
+
+    [HttpPost]
+    public IActionResult Index(Employee employee)
+    {
+        ViewBag.Message = "Employee Saved Successfully";
+
+        return View(employee);
+    }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+
+}
